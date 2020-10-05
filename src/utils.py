fe90a1809a96291a4007b955959c18db39c83f38
gsajko <grzegorz.sajko@protonmail.com>
2020-10-05 18:49:00 +0200

    added getting who I follow

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,27 @@
 import os
 from tweepy import OAuthHandler
 from tweepy import API
-
+from tweepy import Cursor
 
 def twitter_auth():
+    #todo: use json file to store secrets
     consumer_key = os.environ.get("oauth_consumer_key")
     consumer_secret = os.environ.get("oauth_consumer_secret")
+    #todo for now: give least access necessery, if needed, uncomment for good
     # access_token = os.environ.get("oauth_access_token")
     # access_token_secret = os.environ.get("oauth_access_token_secret")
     auth = OAuthHandler(consumer_key, consumer_secret)
     # auth.set_access_token(os.environ["oauth_token"], os.environ["oauth_token_secret"])
-    return API(auth, wait_on_rate_limit=True)
+    #todo return API(auth, wait_on_rate_limit=True)
+    return API(auth)
+
+
+
+def following_ids(screen_name)-> list:
+    api = twitter_auth()
+    following_list = api.friends_ids(screen_name)
+    return following_list
+
+def get_profile(user_id):
+    profile = api.get_user(user_id)._json
+    return profile
--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -4,3 +4,5 @@ import tweepy
 def test_twitter_auth():
     api = utils.twitter_auth()
     assert api is not None
+
+#todo: use test to grab user data, if auth is wrong, it should throw an error
