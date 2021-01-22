820eedb572e9b66ea008c8c9393069528b0ad861
gsajko <grzegorz.sajko@protonmail.com>
2021-01-22 17:17:00 +0100

    added adding to collection

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,36 @@
+import pandas as pd
+import json
 from src.pipeline_functions import prepare_batch
+from src.pipeline_functions import rem_muted, get_collection_id, count_collection, rem_from_collection, processing_list
 
+# TODO leave in memory, don't save as csv
 prepare_batch()
+
+
+owner_id = "143058191"
+
+# remove tweets that are in collection
+custom_newsfeed = get_collection_id(owner_id, "custom_newsfeed")
+count_collection(custom_newsfeed)
+
+while count_collection(custom_newsfeed) > 0:
+    print("removing tweets ...")
+    rem_from_collection(custom_newsfeed)
+
+# load dataframe
+tweets_df = pd.read_csv("src/data/batch_to_add.csv")
+tweets_df = rem_muted(tweets_df, owner_id)
+tweet_list = tweets_df["id"].tolist()[:300]
+
+# TODO remove this- just for checking
+with open("src/data/tweet_list.txt", "w") as write_file:
+    json.dump(tweet_list, write_file)
+
+df = processing_list(custom_newsfeed, tweet_list)
+
+# backup old data
+seen_tweets_old = pd.read_csv("src/data/seen.csv")
+seen_tweets_old.to_csv("src/data/seen_old.csv", index=False)
+
+# update seen.csv file
+df.to_csv("src/data/seen.csv", mode="a", header=False, index=False)
--- src/pipeline_functions.py
+++ src/pipeline_functions.py
@@ -1,10 +1,19 @@
 from datetime import date
 from datetime import timedelta
+import time
+
 import sqlite3
 import pandas as pd
+from twitter_to_sqlite import utils
+
 import re
 import json
 from urllib.parse import urlparse
+
+
+
+auth = json.load(open("auth/auth.json"))
+session = utils.session_for_auth(auth)
 
 # load tweets
 def load_tweets(db_path, days=14):
@@ -19,7 +28,7 @@ def load_tweets(db_path, days=14):
 
 # utils
 def find_url(tweet):
-    return re.findall("http\S+", tweet)
+    return re.findall(r"http\S+", tweet)
 
 def clean_links(tweet):
     tweet = re.sub(r"bit.ly/\S+", "", tweet)
@@ -87,5 +96,101 @@ def prepare_batch():
         .reset_index(drop=True)[:1000]
     )
     to_custom_news_feed[["id", "user"]].to_csv("src/data/batch_to_add.csv")
+
+def count_collection(collection_id, session=session):
+    url = f"https://api.twitter.com/1.1/collections/entries.json?id={collection_id}&count=200"
+    response = session.get(url)
+    collection_tweets = response.json()
+    try:
+        collection_tweets = list(collection_tweets["objects"]["tweets"])
+        if len(collection_tweets) < 100:
+            print(f"{collection_id} contains {len(collection_tweets)} tweets")
+        else:
+            print(f"{collection_id} contains more then 100 tweets")
+        return len(collection_tweets)
+    except:
+        print(f"{collection_id} contains 0 tweets")
+        return 0
+
+
+def get_list_id(owner_id, list_name, session=session):
+    url = f"https://api.twitter.com/1.1/lists/list.json?user_id={owner_id}"
+    response = session.get(url)
+    for l in response.json():
+        if l["name"] == list_name:
+            return l["id"]
+
+
+def get_collection_id(owner_id, collection_name, session=session):
+    url = f"https://api.twitter.com/1.1/collections/list.json?user_id={owner_id}"
+    response = session.get(url)
+    collections = response.json()["objects"]["timelines"]
+    for k in collections.keys():
+        if collections[k]["name"] == collection_name:
+            return k
+
+
+def err_handling(response, sleep=60):
+    while response.reason != "OK":
+        print(response.reason)
+        if response.reason == "Too Many Requests":
+            print(f"Rate limit error - waiting for {sleep} seconds")
+            time.sleep(sleep)
+        else:
+            if "errors" in response.json():
+                print(response.json()["errors"][0]["message"])
+            elif "error" in response.json():
+                print(response.json()["error"])
+            else:
+                print(response.json())
+            raise Exception(f"Status code: {response.status_code}")
+
+
+def rem_from_collection(collection_id, session=session):
+    url = f"https://api.twitter.com/1.1/collections/entries.json?id={collection_id}&count=200"
+    response = session.get(url)
+    collection_tweets = response.json()
+    try:
+        collection_tweets = list(collection_tweets["objects"]["tweets"])
+    except:
+        print(f"{collection_id} collection is empty")
+    for t in collection_tweets:
+        url = f"https://api.twitter.com/1.1/collections/entries/remove.json?id={collection_id}&tweet_id={t}"
+        response = session.post(url)
+        err_handling(response)
+
+
+
+def processing_list(collection_id, tweet_list):
+    collection_id = collection_id
+    procc_list = []
+    print("adding tweets to collection {collection_id}")
+    for counter, tweet_id in enumerate(tweet_list):
+        if (counter + 1) % 20 == 0:
+            print(f"{(counter+1)} / {len(tweet_list)}")
+        url = f"https://api.twitter.com/1.1/collections/entries/add.json?tweet_id={tweet_id}&id={collection_id}"
+        response = session.post(url)
+        err_handling(response)
+        if response.reason == "OK":
+            errors = response.json()["response"]["errors"]
+            if len(errors) > 0:
+                procc_list.append(
+                    {"tweet_id": tweet_id, "err_reason": errors[0]["reason"]}
+                )
+            else:
+                procc_list.append({"tweet_id": tweet_id, "err_reason": "no_errors"})
+    df = pd.DataFrame(procc_list)
+    print(df["err_reason"].value_counts())
+    return df
+
+
+
+def rem_muted(df, owner_id, session=session):
+    muted_list = get_list_id(owner_id, "muted")
+    url = f"https://api.twitter.com/1.1/lists/members.json?list_id={muted_list}&owner_id={owner_id}"
+    response = session.get(url)
+    muted_accounts = [i["id"] for i in response.json()["users"]]
+    df = df[~df["user"].isin(muted_accounts)]
+    return df
 
 
