a78b7a33eb956716768c27ba02506a347733a18a
gsajko <grzegorz.sajko@protonmail.com>
2021-02-05 19:19:50 +0100

    init tests

---
+++ tests/test_pipelines.py
@@ -0,0 +1,6 @@
+# test_pipelines
+# Test src/pipeline_functions.py and src/pipeline.py components
+# first create db of tweets
+# twitter-to-sqlite home-timeline home.db -a /home/gsajko/work/custom_twitter_feed/auth/auth.json --since
+
+from pipeline_functions import load_tweets
