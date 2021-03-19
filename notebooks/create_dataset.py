8c1bc6464c381960ad4139a199498dd949b66425
gsajko <grzegorz.sajko@protonmail.com>
2021-03-19 23:21:19 +0100

    analyzed likes

--- notebooks/create_dataset.py
+++ notebooks/create_dataset.py
@@ -1,5 +1,18 @@
 # %%
-from tweetfeed.data import load_tweets
+import json
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from tweetfeed.data import concat_tweet_text, find_news, load_tweets
+from tweetfeed.twitter_utils import (
+    get_collection_id,
+    get_tweets_from_collection,
+)
+
+# %matplotlib inline
 
 # %%
 ## Load tweets
@@ -14,26 +27,90 @@ df_tweets.describe()
 ## load tweets
 ## eda
 ### most liked
-### most retweeted
-### nr of hastags
-### how many retweets?
-### tweet lenght
-### lang?
+# %%
+df_tweets.sort_values(by="favorite_count", ascending=False)
+# %%
+g = sns.displot(data=df_tweets.favorite_count, bins=50)
+plt.ticklabel_format(style="plain", axis="x")
+g.set_axis_labels("Likes", "# Tweets")
+# %%
+# retweets don't have likes
+df_nRT = df_tweets[df_tweets["retweeted_status"] == "N/A"]
+g = sns.displot(data=df_nRT.favorite_count, bins=50)
+plt.ticklabel_format(style="plain", axis="x")
+g.set_axis_labels("Likes", "# Tweets")
+# %%
+g = sns.displot(
+    data=df_nRT.favorite_count[df_nRT.favorite_count < 200], bins=50
+)
+plt.ticklabel_format(style="plain", axis="x")
+g.set_axis_labels("Likes", "# Tweets")
+
+# %%
+log_likes = np.log1p(df_nRT.favorite_count)
+l = sns.displot(data=log_likes, bins=50)
+l.set_axis_labels("Log(Likes + 1)", "# Tweets")
+
+# %%
+df_nRT.sort_values(by="favorite_count", ascending=False).head(20)
+# %%
 
 # Dataset= seen.csv+negative+positive
 
 ### load seen
-
+# %%
+reviewed_tweets = pd.read_csv("../tweetfeed/data/seen.csv")
+# %%
+df_seen = df_tweets[df_tweets["id"].isin(reviewed_tweets["tweet_id"].tolist())]
+# %%
+g = sns.displot(data=df_seen.favorite_count, bins=50)
+plt.ticklabel_format(style="plain", axis="x")
+g.set_axis_labels("Likes", "# Tweets")
+# %%
+g = sns.displot(
+    data=df_seen.favorite_count[df_seen.favorite_count < 200], bins=50
+)
+plt.ticklabel_format(style="plain", axis="x")
+g.set_axis_labels("Likes", "# Tweets")
+# %%
+log_likes = np.log1p(df_seen.favorite_count)
+l = sns.displot(data=log_likes, bins=50)
+l.set_axis_labels("Log(Likes + 1)", "# Tweets")
+# %%
+df_seen.sort_values(by="favorite_count", ascending=False)["full_text"].head(20)
+# %%
 ## negative
+auth: str = "../config/auth.json"
+owner_id: str = "143058191"
 ### load from not_relevant
-### find_news_in_url
+not_relevant_list = get_tweets_from_collection(
+    get_collection_id(owner_id, auth, "not_relevant"), auth
+)
+# %%
+# prep_batch
+df = concat_tweet_text(df_tweets)
+df = df[df["retweeted_status"] == "N/A"]
+with open("../tweetfeed/data/news_domains.txt", "r") as f:
+    news_domains = json.loads(f.read())
+df = find_news(df, news_domains)
+df_news = df[df["contains_news"] == 1]
+# %%
+
 ### use muted lists
+# TODO first make muted functions
 
 ## neutral
 ### seen - not interacted with
 
 ## positive - interactions, likes and comments
 ### load likes
+
+
+## EDA
+### nr of hastags
+### how many retweets?
+### tweet lenght
+### lang?
 
 ## work on "full text"
 ### concat with replies to... RT
