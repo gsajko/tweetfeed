cb73027be88fd321806fcd53c1e741b895b940af
Grzegorz Sajko <grzegorz.sajko@protonmail.com>
2022-06-09 15:21:03 +0200

    migration to postgres

--- airflow/dags/get_other_tweets1.py
+++ airflow/dags/get_other_tweets1.py
@@ -10,7 +10,7 @@ from airflow.operators.bash import BashOperator
     schedule_interval="27 * * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    dagrun_timeout=datetime.timedelta(minutes=180),
+    dagrun_timeout=datetime.timedelta(minutes=10),
     tags=["cron", "twitter-to-sql"],
 )
 def get_other_tweets():
@@ -19,11 +19,11 @@ def get_other_tweets():
         task_id="faves_to_faves",
         bash_command=f"run-one /usr/local/bin/twitter-to-sqlite favorites /home/sjao/work/tweetfeed/data/faves.db -a /home/sjao/work/tweetfeed/config/auth.json && sleep {sleep_time}",
     )
-    timeline_to_timelinedb = BashOperator(
-        task_id="timeline_to_timelinedb",
-        bash_command=f"run-one /usr/local/bin/twitter-to-sqlite user-timeline /home/sjao/work/tweetfeed/data/timeline.db -a /home/sjao/work/tweetfeed/config/auth.json --since && sleep {sleep_time}",
+    timeline_to_homedb = BashOperator(
+        task_id="timeline_to_home",
+        bash_command=f"run-one /usr/local/bin/twitter-to-sqlite user-timeline /home/sjao/work/tweetfeed/data/home.db -a /home/sjao/work/tweetfeed/config/auth.json --since && sleep {sleep_time}",
     )
-    faves_to_favesdb >> timeline_to_timelinedb
+    faves_to_favesdb >> timeline_to_homedb
 
 other_tweets_dag = get_other_tweets()
 
