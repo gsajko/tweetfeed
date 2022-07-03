289c70cfdfe334f07a3253743b9b2d5b29c2c48f
Grzegorz Sajko <grzegorz.sajko@protonmail.com>
2022-07-03 21:31:09 +0200

    st fix

--- streamlit/st_app.py
+++ streamlit/st_app.py
@@ -1,6 +1,11 @@
 import sys
-sys.path.append("app/tweetfeed/")
-from tweetfeed import twitterutils
 import streamlit as st
+sys.path.append("app/tweetfeed/")
+try:
+    from tweetfeed import twitterutils
+except Exception as e:
+    st.write(e)
+
+
 st.write(sys.path)
 
