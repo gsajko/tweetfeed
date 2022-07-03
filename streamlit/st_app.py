926f4bfeb34e8c304c2abbc8d46720ff26f7424a
Grzegorz Sajko <grzegorz.sajko@protonmail.com>
2022-07-03 21:34:35 +0200

    st fix

--- streamlit/st_app.py
+++ streamlit/st_app.py
@@ -1,11 +1,12 @@
 import sys
+import os
 import streamlit as st
 sys.path.append("app/tweetfeed/tweetfeed")
 
 
 
 st.write(sys.path)
-
+st.write(os.path.dirname())
 try:
     from tweetfeed import twitterutils
 except Exception as e:
