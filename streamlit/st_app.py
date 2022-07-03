1019b87808a01d098880c7b81c6cb528a1f30afd
Grzegorz Sajko <grzegorz.sajko@protonmail.com>
2022-07-03 21:32:06 +0200

    st fix

--- streamlit/st_app.py
+++ streamlit/st_app.py
@@ -1,11 +1,12 @@
 import sys
 import streamlit as st
-sys.path.append("app/tweetfeed/")
-try:
-    from tweetfeed import twitterutils
-except Exception as e:
-    st.write(e)
+sys.path.append("app/tweetfeed/tweetfeed")
+
 
 
 st.write(sys.path)
 
+try:
+    from tweetfeed import twitterutils
+except Exception as e:
+    st.write(e)
